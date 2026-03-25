13bf009ca80c6fc6c94f622f11b498348d5b6e5f
Dhanesh Surya <dhanesh.surya@gmail.com>
2026-03-25 15:15:25 +0530

    Fix ALLOWED_HOSTS and DEBUG for local development

--- result_portal/settings.py
+++ result_portal/settings.py
@@ -11,9 +11,9 @@ SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY", "django-insecure-default-key-change-in-production"
 )
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
